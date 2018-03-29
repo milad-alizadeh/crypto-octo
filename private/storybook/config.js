747d5040f1c54fa22123de22e3668f37dce47959
Milad Alizadeh <milaadd@gmail.com>
2018-03-29 11:50:14 +0100

    update storybook settings

--- private/storybook/config.js
+++ private/storybook/config.js
@@ -1,18 +1,28 @@
-// https://github.com/diegohaz/arc/wiki/Storybook
-import React from 'react'
-import { configure, addDecorator } from '@storybook/react'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from 'styled-components'
-import configureStore from 'store/configure'
-import api from 'services/api'
-import theme from 'components/themes/default'
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, injectGlobal } from 'styled-components';
+import configureStore from 'store/configure';
+import api from 'services/api';
+import theme from 'components/themes/default';
+import cssReset from 'components/css-reset';
 
-const store = configureStore({}, { api: api.create() })
-const req = require.context('components', true, /.stories.js$/)
+const store = configureStore({}, { api: api.create() });
+const req = require.context('components', true, /.stories.js$/);
+
+// CSS Reset
+cssReset();
+
+injectGlobal`
+  body {
+    background: ${theme.colors.greyDarkest};
+    padding: 20px;
+  }
+`;
 
 function loadStories() {
-  req.keys().forEach(filename => req(filename))
+  req.keys().forEach(filename => req(filename));
 }
 
 addDecorator(story => (
@@ -21,6 +31,6 @@ addDecorator(story => (
       <ThemeProvider theme={theme}>{story()}</ThemeProvider>
     </BrowserRouter>
   </Provider>
-))
+));
 
-configure(loadStories, module)
+configure(loadStories, module);
