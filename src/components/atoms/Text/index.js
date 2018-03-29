3d921f5b31522d638544cefc99d9470b60fc018c
Milad Alizadeh <milaadd@gmail.com>
2018-03-29 17:05:03 +0100

    refactor tet component

--- src/components/atoms/Text/index.js
+++ src/components/atoms/Text/index.js
@@ -5,15 +5,14 @@ import PropTypes from 'prop-types';
 const TextStyled = styled.span`
   font-size: 1.4rem;
   text-transform: uppercase;
+  letter-spacing: .03em;
   font-family: ${({ theme }) => theme.fonts.secondary};
   color: ${({ theme, color }) => color ? theme.colors[color] : theme.colors.greyLight};
 `;
 
-const Text = (props) => {
-  return (
-    <TextStyled {...props}>{ props.children }</TextStyled>
-  );
-};
+const Text = props => (
+  <TextStyled {...props}>{ props.children }</TextStyled>
+);
 
 Text.propTypes = {
   children: PropTypes.node,
