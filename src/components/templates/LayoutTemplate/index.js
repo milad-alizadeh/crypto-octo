c5d124828d0a3f91320aace2637f531cbdc3a53b
Milad Alizadeh <milaadd@gmail.com>
2018-04-06 04:38:03 +0100

    add styling to layouttemplate

--- src/components/templates/LayoutTemplate/index.js
+++ src/components/templates/LayoutTemplate/index.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
+import breakpoint from 'styled-components-breakpoint';
 import PropTypes from 'prop-types';
 
 const LayoutTemplateStyled = styled.div`
   display: grid;
+  max-width: 100vw;
+  min-height: 100vh;
+  grid-template-rows: 5rem auto 1fr 5rem;
+  grid-template-areas:
+    "header"
+    "nav"
+    "main"
+    "footer";
+  ${breakpoint('xlarge')`
+    grid-template-rows: 5rem 1fr 5rem;
+    grid-template-columns: 18rem 1fr;
+    grid-template-areas:
+      "header header"
+      "nav main"
+      "nav footer";
+  `}
 `;
 
-const Header = styled.header``;
-const Main = styled.main``;
-const Footer = styled.footer``;
-const Nav = styled.nav``;
+const Header = styled.header`
+  grid-area: header;
+  background: ${({ theme }) => theme.colors.greyDarker};
+
+  ${breakpoint('xlarge')`
+    background: ${({ theme }) => theme.colors.greyDark};
+  `}
+`;
+
+const Nav = styled.nav`
+  grid-area: nav;
+  background: ${({ theme }) => theme.colors.greyDark};
+`;
+
+const Main = styled.main`
+  grid-area: main;
+`;
+
+const Footer = styled.footer`
+  grid-area: footer;
+  background: ${({ theme }) => theme.colors.greyDark};
+`;
 
 const LayoutTemplate = ({ header, footer, nav, children }) => {
   return (
